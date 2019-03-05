fb2ab5daed9a8bc3d086b15e1df8c0bcce31700b
Shashwat Priyadarshi <shshwt25@users.noreply.github.com>
2019-03-05 18:55:24 +0530

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -2,49 +2,65 @@
 
 __author__ = "bt3"
 
-def binary_search(array, value):   
-    last, first = len(array), 0
-    
-    while first < last:
-        mid = (last - first)//2
-        item = array[mid]
-        
-        if item == value:
-            return True
-        
-        elif item < value:
-            last = mid
-        
+
+def binary_search(array, item, hi=None, lo=0):
+    '''
+    >>> binary_search([2,3,5,6,8,10,15,23], 15)
+    6
+    >>> binary_search([2,3,5,6,8,10,15,23], 4)
+    False
+    >>> binary_search([1,3,4,5,7,8 ,10,12,23], 10)
+    6
+    >>> binary_search([1,3,4,5,7,8 ,10,12,23], 22)
+    False
+    '''
+
+    hi = hi or len(array)
+    if hi < lo:
+        return False
+
+    mid = (hi + lo)//2
+    if  item == array[mid]:
+        return mid
+    elif item < array[mid]:
+        return binary_search(array, item, hi=mid-1, lo=lo)
+    else:
+        return binary_search(array, item, hi=hi, lo=mid+1)
+
+
+
+
+def binary_search_iter(array, item):
+    '''
+    >>> binary_search_iter([2,3,5,6,8,10,15,23], 15)
+    6
+    >>> binary_search_iter([2,3,5,6,8,10,15,23], 4)
+    False
+    >>> binary_search_iter([1,3,4,5,7,8 ,10,12,23], 10)
+    6
+    >>> binary_search_iter([1,3,4,5,7,8 ,10,12,23], 22)
+    False
+    '''
+    lo, hi = 0, len(array)
+
+    while lo < hi:
+        mid = (hi+lo)//2
+        if array[mid] == item:
+            return mid
+        elif array[mid] > item:
+            hi = mid
         else:
-            first = mid 
-    
+            lo=mid+1
     return False
 
-def binary_search_rec(array, value, first=0, last=None):
-    last = last or len(array)
-    if len(array[first:last]) < 1:
-        return False
-    
-    mid = (last - first)//2
-    if array[mid] == value:
-        return True
-    elif array[mid] < value:
-        return binary_search_rec(array, value, first=first, last=mid)
-    else:
-        return binary_search_rec(array, value, first=mid, last=last)
 
-    
-if __name__ == '__main__':    
-    array = [3, 4, 6, 7, 10, 11, 34, 67, 84]
-    value = 6
-    assert(binary_search(array, value) == True)   
-    assert(binary_search_rec(array, value) == True)  
-    value = 8
-    assert(binary_search(array, value) == False)
-    assert(binary_search_rec(array, value) == False)  
-    array = [8]
-    assert(binary_search(array, value) == True)
-    assert(binary_search_rec(array, value) == True)  
-    array = []
-    assert(binary_search(array, value) == False)
-    assert(binary_search_rec(array, value) == False)  
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
